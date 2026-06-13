15c1e1af01b5cfb2979a210111c01401b67219ea
Sylverb <sylver.bruneau@gmail.com>
2026-06-13 11:06:09 +0200

    Removed useless ahb malloc

--- Src/Emulator/Properties.c
+++ Src/Emulator/Properties.c
@@ -461,7 +461,7 @@ Properties* propCreate(int useDefault, int langType, PropKeyboardLanguage kbdLan
 #ifndef TARGET_GNW
     properties = malloc(sizeof(Properties));
 #else
-    properties = ahb_malloc(sizeof(Properties));
+    properties = globalProperties;
 #endif
 
     if (globalProperties == NULL)
